bc1c339e58da03102ba4827c5d3552bf72936d4a
Yana <yanaegorova69.3@gmail.com>
2022-01-29 02:24:47 +0200

    +

---
+++ js/templates/product-item.js
@@ -0,0 +1,30 @@
+export const item = (product) => {
+
+  return `
+    <li class="item">
+      
+  <div class="product__block js_product glass glass-m" id="${product.id}">
+
+    <div class="product__img-box">
+    
+     <img src="${product.image}" alt="" class="img">
+
+    </div>
+    <div class="product__text-box-wrapper">
+    <div class="product__text-box">
+    <p class="product__name js_product-name">${product.name}</p>
+      <span class="product__price">
+      $${(product.price).toFixed(2)}
+      </span>
+      <div class="product__btn-block">
+      <a href="product-page.html?id=${product.id}" class="btn">details</a>
+      <button class="btn js_add-to-cart">buy</button>
+      </div>
+    </div>
+</div>
+</div>
+  </li>
+    `
+}
+
+
